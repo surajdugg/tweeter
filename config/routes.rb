8f5585a2f29142cd563f1835007431ffd5742e04
Suraj Duggirala <surajdugg@gmail.com>
2013-12-11 13:41:19 +0530

    Styling added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ SampleApp::Application.routes.draw do
   get "pages/home"
   get "pages/contact"
   get "pages/about"
+  # match '/about', to: 'pages#about', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
